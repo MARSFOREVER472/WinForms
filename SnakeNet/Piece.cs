e31233d1af6ca13491d1e2d22e1aa9430e7ea8ed
Marcelo Andrés <marcelo.lam@alumnos.ucn.cl>
2023-07-18 13:22:20 -0400

    Se agregaron clases al juego.

---
+++ SnakeNet/Piece.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms; // Agregar una librería para los componentes de la interfaz.
+
+namespace SnakeNet
+{
+    class Piece: Label // Clase Piece mediante un componente de texto.
+    {
+        public Piece(int x, int y) // La serpiente tiene dos dimensiones (ancho y altura).
+        {
+            Location = new System.Drawing.Point(x, y); // Se ubicará la posición en donde se encuentra la serpiente.
+            Size = new System.Drawing.Size(20, 20); // El tamaño de la comida será la misma que la original.
+            BackColor = Color.Orange; // El color de la serpiente es naranja.
+            Enabled = false; // Por el momento no se activará la visualización de la serpiente.
+        }
+    }
+}
---
+++ SnakeNet/Snake.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeNet
+{
+    public partial class Snake : Form
+    {
+        // A la serpiente le pasamos varios parámetros de acción.
+
+        int cols = 50, rows = 25, score = 0, dx = 0, dy = 0, front = 0, back = 0;
+        Piece[] snake = new Piece[1250]; // La serpiente tendrá el tamaño de sus partes por defecto.
+        List<int> available = new List<int>(); // Se encontrará disponible mediante lista las partes de la serpiente.
+        bool[,] visit; // Estará o no la serpiente.
+
+        Random rand = new Random(); // Variable Aleatoria.
+        Timer temporizador = new Timer(); // Contador de tiempo.
+
+        // EN INSTANTES...
+        public Snake()
+        {
+            InitializeComponent();
+            // EN INSTANTES...
+        }
+
+        private void Snake_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
