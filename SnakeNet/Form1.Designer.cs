55c1b9ac5d1222c3251b5e53850a280197217ac5
Marcelo Andrés <marcelo.lam@alumnos.ucn.cl>
2023-07-18 13:00:28 -0400

    Consejo: Cambiar el tamaño de la comida desactivando el modo AutoSize.

---
+++ SnakeNet/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace SnakeNet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblComida = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(12, 9);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(235, 31);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "Puntuación: 0";
+            // 
+            // lblComida
+            // 
+            this.lblComida.BackColor = System.Drawing.Color.Red;
+            this.lblComida.Location = new System.Drawing.Point(478, 230);
+            this.lblComida.Name = "lblComida";
+            this.lblComida.Size = new System.Drawing.Size(21, 21);
+            this.lblComida.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 500);
+            this.Controls.Add(this.lblComida);
+            this.Controls.Add(this.lblScore);
+            this.Font = new System.Drawing.Font("Courier New", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblComida;
+    }
+}
+
